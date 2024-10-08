4ab50f603ade4bad5cd322ee6999c906bbf6dc00
Abhinav Kompella <kompellabhinav@gmail.com>
2024-10-08 18:11:25 -0400

    Finished basic setup

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:in_class_group_challenge/game_screen.dart';
+void main() {
+  runApp(HalloweenGame());
+}
+
+class HalloweenGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Halloween Game',
+      theme: ThemeData.dark(),
+      home: GameScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
